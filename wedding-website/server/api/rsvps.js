3131ea2ba2ad1f01fc66da4bace81455c9acb4e9
Anna Morozova <anna.mik.morozova@gmail.com>
2020-08-29 23:56:56 -0400

    Feat: rsvps routes

---
+++ wedding-website/server/api/rsvps.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const {RSVP} = require("../db/models");
+
+app.post("/", (req, res) => 
+    RSVP.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        plusOneFirstName: req.body.plusOneFirstName,
+        plusOneLastName: req.body.plusOneLastName,
+        attendance: req.body.attendance
+    }).then( (result) => res.json(result) )
+  );
+
+module.exports = router;
