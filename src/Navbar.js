7be5347bcbc08f570347ecd961a069a1ba07d74e
Anna Morozova <anna.mik.morozova@gmail.com>
2020-09-06 23:34:43 -0400

    Style: header size

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
- 
+import headerPicture from "./header.jpg" 
+
 function Navbar() {
   
   return (
     <div className="App">
-      <header className="header-img">
-      </header>
+      <img src={headerPicture} alt="headerPicture" className="header-img">
+      </img>
       <div className="row-navbar">
           <a href="/" className="navbar-style grow">Erik + Anna</a>
           <a href="/venue" className="navbar-style grow">Venue</a>
