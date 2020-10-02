721dfa0660b20534010a499d9cad55a06020eddc
Anna Morozova <anna.mik.morozova@gmail.com>
2020-10-02 19:06:56 -0400

    Feat: add secret songs route

--- server/api/songs.js
+++ server/api/songs.js
@@ -5,7 +5,16 @@ router.post("/", (req, res) => {
   Song.create({
       name: req.body.name,
       artist: req.body.artist
-  }).then( (result) => res.json(result) )
+  }).then( (result) => res.json(result))
+});
+
+router.get("/", async (req, res, next) => {
+	try {
+		const songs = await Song.findAll();
+		res.json(songs);
+	} catch (error) {
+		next(error);
+	}
 });
 
 module.exports = router;
